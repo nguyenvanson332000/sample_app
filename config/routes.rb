74a6b57301133a862bca7cf901f43a5da02bb913
nguyenvanson332000 <nguyenvanson332000@gmail.com>
2021-11-03 08:28:53 -0700

    Chap 7: Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get "static_pages/home",to: "static_pages#home"
   get "static_pages/help",to: "static_pages#help"
   get "static_pages/about",to: "static_pages#about"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(create new show)
 end
