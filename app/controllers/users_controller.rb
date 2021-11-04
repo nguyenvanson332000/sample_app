e79099ca32b92a2beffa38b50e2de7fb854feaca
nguyenvanson332000 <nguyenvanson332000@gmail.com>
2021-11-04 04:02:40 -0700

    Chap 8: Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t "welcome__message", username: @user.name
+      log_in @user
+      flash[:success] = t "welcome_message", username: @user.name
       redirect_to @user
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save{self.email = email.downcase}
+  before_save{email.downcase!}
   validates :name, presence: true,
                    length: {maximum: Settings.max_length.name_50}
   VALID_EMAIL_REGEX = /\A[\w\-.+]+@[a-z\-\d.]+\.[a-z]+\z/i.freeze
@@ -10,4 +10,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,
                        length: {minimum: Settings.min_length.password_6}
+
+  # Returns the hash digest of the given string.
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create string, cost: cost
+  end
 end
